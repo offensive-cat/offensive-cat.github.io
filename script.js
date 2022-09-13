71af181b9eecaa4d2c1946f32f60ddc3975809f6
stonycat <39594753+stonycat0201@users.noreply.github.com>
2022-09-13 08:43:40 +0700

    Adding "Dark/Light toggle" feature

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 function darkLightToggle() {
    var element = document.body
-   var imgElement = document.getElementById('footer-icon');
-
    var imgDark = "./assets/icon/Nuclear's WM 2022 (dark mode).png"
    var imgLight = "./assets/icon/Nuclear's WM 2022 (light mode).png"
 
+   let imgElement = document.getElementById("footer-icon");
+   
+   //change theme
    element.classList.toggle("light-mode")
 
-   imgElement.src.toggle(imgLight)
+   //change footer logo "Nuclear 2022"
+   if (imgElement.getAttribute('src') === imgDark) {
+      imgElement.setAttribute('src', imgLight);
+    }
+    else {
+      imgElement.setAttribute('src', imgDark);
+    }
 }
